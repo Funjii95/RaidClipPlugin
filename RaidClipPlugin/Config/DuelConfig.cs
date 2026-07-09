888130eb7d43bd9e30558284c6f64eefba0e15b8
Funjii95 <benjaminmosig@gmail.com>
2026-07-09 21:04:54 +0200

    Repair UTF-8 text in DuelConfig.cs

--- RaidClipPlugin/Config/DuelConfig.cs
+++ RaidClipPlugin/Config/DuelConfig.cs
@@ -28,15 +28,15 @@ public sealed class DuelConfig
     public int LoserTimeoutSeconds { get; set; } = 60;
     public string LoserTimeoutReason { get; set; } = "Duel verloren";
     public string DuelRequestMessage { get; set; } = "@{challenger} fordert @{target} zu einem Duel um {amount} {currencyName} heraus! @{target}, antworte mit {acceptCommand} oder {denyCommand}. Zeit: {seconds} Sekunden.";
-    public string DuelAcceptedMessage { get; set; } = "@{target} nimmt das Duel gegen @{challenger} an! Der Pot betrÃ¤gt {pot} {currencyName}.";
-    public string DuelWinMessage { get; set; } = "Das Duel ist entschieden! @{winner} gewinnt gegen @{loser} und erhÃ¤lt {pot} {currencyName}!";
-    public string DuelDeniedMessage { get; set; } = "@{target} hat das Duel gegen @{challenger} abgelehnt. Der Einsatz wurde zurÃ¼ckgegeben.";
-    public string DuelTimeoutMessage { get; set; } = "Das Duel zwischen @{challenger} und @{target} ist abgelaufen. Der Einsatz wurde zurÃ¼ckgegeben.";
-    public string NotEnoughPointsChallengerMessage { get; set; } = "@{challenger}, du hast nicht genug {currencyName} fÃ¼r dieses Duel.";
+    public string DuelAcceptedMessage { get; set; } = "@{target} nimmt das Duel gegen @{challenger} an! Der Pot beträgt {pot} {currencyName}.";
+    public string DuelWinMessage { get; set; } = "Das Duel ist entschieden! @{winner} gewinnt gegen @{loser} und erhält {pot} {currencyName}!";
+    public string DuelDeniedMessage { get; set; } = "@{target} hat das Duel gegen @{challenger} abgelehnt. Der Einsatz wurde zurückgegeben.";
+    public string DuelTimeoutMessage { get; set; } = "Das Duel zwischen @{challenger} und @{target} ist abgelaufen. Der Einsatz wurde zurückgegeben.";
+    public string NotEnoughPointsChallengerMessage { get; set; } = "@{challenger}, du hast nicht genug {currencyName} für dieses Duel.";
     public string NotEnoughPointsTargetMessage { get; set; } = "@{target}, du hast nicht genug {currencyName}, um dieses Duel anzunehmen.";
     public string SelfDuelMessage { get; set; } = "@{user}, du kannst dich nicht selbst duellieren.";
     public string NoPendingDuelMessage { get; set; } = "@{user}, du hast aktuell keine offene Duel-Anfrage.";
-    public string WrongTargetMessage { get; set; } = "@{user}, diese Duel-Anfrage ist nicht fÃ¼r dich bestimmt.";
+    public string WrongTargetMessage { get; set; } = "@{user}, diese Duel-Anfrage ist nicht für dich bestimmt.";
     public string AlreadyPendingDuelMessage { get; set; } = "@{user}, du hast bereits eine offene Duel-Anfrage.";
     public string InvalidBetMessage { get; set; } = "@{user}, bitte nutze: {duelCommand} <user> <punkte>";
 }
